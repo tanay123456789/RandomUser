73b652509351086eb09b56219750996a93989a37
Tanay Dwivedi <tanaydwivedi456@gmail.com>
2021-08-08 13:01:40 +0530

    feature:added title,firstname,lastname

--- my-app/src/MyCard.js
+++ my-app/src/MyCard.js
@@ -7,8 +7,17 @@ const MyCard=({details})=>{
         <Card>
             <CardBody className="text-center">
                 <img height="150" width="150"
-                className="rounded-circle img-thumbnail border-danger"/>
-                src={details.picture?.large}
+                className="rounded-circle img-thumbnail border-danger"
+                src={details.picture?.large}/>
+                <CardTitle className="text-primary">
+                    <h1><span className="pr-4">{details.name?.title}</span>
+                    <span>{details.name?.first}</span>
+                    <span>{details.name?.last}</span>
+
+
+
+                    </h1>
+                </CardTitle>
             </CardBody>
         </Card>
     )
