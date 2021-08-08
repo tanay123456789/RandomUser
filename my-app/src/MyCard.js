90ffad8f70c6614280a72a2d52bdac29ce4266a2
Tanay Dwivedi <tanaydwivedi456@gmail.com>
2021-08-08 12:05:53 +0530

    fix:a typo in MyCard component

--- my-app/src/MyCard.js
+++ my-app/src/MyCard.js
@@ -2,13 +2,13 @@ import React from "react"
 import {Card,CardBody,CardTitle,CardText} from "reactstrap"
 import {FaEnvelope,FaMapMarkedAlt,Faphone} from "react-icons/fa"
 
-const Card=()=({details})=>{
+const Card=({details})=>{
     return(
         <Card>
             <CardBody className="text-center">
                 <img height="150" width="150"
                 className="rounded-circle img-thumbnail border-danger"/>
-                src={details.picture.large}
+                src={details.picture?.large}
             </CardBody>
         </Card>
     )
